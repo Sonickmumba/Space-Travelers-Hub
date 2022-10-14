ef74026a8892010e5a23d3a02652c9358d003fca
Tommymarvel <99184522+Tommymarvel@users.noreply.github.com>
2022-10-14 17:35:05 +0100

    Update mission.js
    Added reserve button

--- src/redux/Missions/mission.js
+++ src/redux/Missions/mission.js
@@ -1,22 +1,38 @@
-import { type } from '@testing-library/user-event/dist/type';
-
 const READ = 'spaceTravel/missions/READ';
-const BOOK = 'spaceTravel/missions/BOOK';
-const LEAVE = 'spaceTravel/missions/BOOK';
+const BOOK = "spaceTravel/missions/BOOK";
+const LEAVE = 'spaceTravel/missions/LEAVE';
 
 export const read = (missions) => ({
   type: READ,
   payload: missions,
 });
 
-export const book = (id) => {
-  type;
-};
+export const book = (id) => ({
+  type: BOOK,
+  payload: id
+});
+
+export const leave = (id) => ({
+  type: LEAVE,
+  payload: id,
+});
 
 const missionsReducer = (state = [], action) => {
   switch (action.type) {
     case READ:
       return action.payload;
+    case BOOK:
+      newState = state.map((item) => {
+        if (item.mission_id !== action.payload) return item;
+        return { ...item, reserved: true };
+      });
+      return newState
+    case LEAVE:
+         newState = state.map((item) => {
+        if (item.mission_id !== action.payload) return item;
+        return { ...item, reserved: false };
+      });
+      return newState;
     default:
       return state;
   }
@@ -31,6 +47,7 @@ export const joinMissions = () => async (dispatch) => {
           mission_id: missions[key].mission_id,
           name: missions[key].mission_name,
           description: missions[key].description,
+          reserved: false,
         });
         return missionsList;
       });
