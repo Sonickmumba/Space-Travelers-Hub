95e3871a081502b0ea56e128c38e55cd7281b428
sonickmumba <kingellie.mumba@gmail.com>
2022-10-16 00:03:44 +0200

    Add color for active link

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -6,32 +6,24 @@ const NavLinks = () => {
     display: 'flex',
     listStyleType: 'none',
   };
-  const navLink = {
-    textDecoration: 'none',
-    width: '3.688rem',
-    height: '1rem',
-    margin: '1.125rem 2.063rem 0.688rem 0',
-    fontSize: '1rem',
-    letterSpacing: '1.9px',
-    color: '#0290ff',
-  };
 
   const line = {
     borderRight: '2px solid black',
     marginRight: '2.563rem',
+    marginLeft: '2.563rem',
   };
   return (
     <nav id="navbar-container">
       <ul style={navStyle}>
         <li className="nav-link">
-          <NavLink to="/" style={navLink} id="navLink">Rockets</NavLink>
+          <NavLink to="/" id="navLink" activeClassName="active">Rockets</NavLink>
         </li>
         <li className="nav-link">
-          <NavLink to="/mission" style={navLink}>Missions</NavLink>
+          <NavLink to="/mission" activeClassName="active">Missions</NavLink>
         </li>
         <span style={line} />
         <li className="nav-link">
-          <NavLink to="/profile" style={navLink}>My Profile</NavLink>
+          <NavLink to="/profile" activeClassName="active">My Profile</NavLink>
         </li>
       </ul>
     </nav>
